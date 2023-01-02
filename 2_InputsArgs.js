dba884463aaa55904da01429fb5f3f24b267aa58
karim Med <mr.medar.karim@gmail.com>
2023-01-02 15:29:23 +0100

    ex1 & ex2

---
+++ 2_InputsArgs.js
@@ -0,0 +1,5 @@
+function inputsArgs(par1, par2) {
+  return par2;
+}
+
+module.exports = inputsArgs;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const example = require("./1_Example.js");
+
+console.log(example());
+
+const inputsArgs = require("./2_InputsArgs.js");
+console.log(inputsArgs(42, 84));

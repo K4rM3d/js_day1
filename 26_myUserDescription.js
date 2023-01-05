0746a168aa1804f751923d52176970c92c645e7b
karim Med <mr.medar.karim@gmail.com>
2023-01-05 14:04:26 +0100

    update ex26

--- 26_myUserDescription.js
+++ 26_myUserDescription.js
@@ -1,5 +1,5 @@
 function myUserDescription(objet = { surname: medar, name: karim, age: 30 }) {
-  return `je m'apelle ${objet.surname} ${objet.name}, j'ai ${objet.age} ans`;
+  return `Je m'apelle ${objet.surname} ${objet.name}, j'ai ${objet.age} ans`;
 }
 
 module.exports = myUserDescription;
